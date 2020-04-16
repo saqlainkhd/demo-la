1513e6e79dc1d8693d4da4207a5f112be7e28b1a
Saqlain Khalid <saqlain09e@gmail.com>
2020-04-16 17:52:26 +0500

    Genrate Question Model and setup a relationshiop with user table

---
+++ app/Question.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+
+    protected $fillable = ['title','body'];
+
+    public function user(){
+        return $this->belongTo(User::class);
+    }
+
+
+    public function setTitleAttribute($value)
+    {
+      $this->attribute['title'] = $value;
+      $this->attribute['slug'] = str_slug($value);
+    }
+}
+
